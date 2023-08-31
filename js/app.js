1f4f82e1fd207581c0b9dadeae13974f769cdb1c
Arahman75 <selfarahman01825@gmail.com>
2023-08-31 21:02:55 +0600

    added a category

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('hello world!');
+const handleCategory = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const data = await res.json()
+    const categories = data.data;
+    console.log(categories);
+
+    categories.forEach(category => {
+        console.log(category.category);
+        const dataContainer = document.getElementById('data-container');
+        const div = document.createElement('div');
+        div.innerHTML = `
+      <a class="bg-gray-200 py-1 px-3 text-lg cursor-pointer">${category.category}</a>`;
+        dataContainer.appendChild(div);
+    });
+
+}
+handleCategory()
