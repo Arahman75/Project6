e16d0abc327bd5c36f74581bf1a895686dbb660a
Arahman75 <selfarahman01825@gmail.com>
2023-09-01 18:06:00 +0600

    added a blog page

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const handleCategory = async () => {
         const dataContainer = document.getElementById('data-container');
         const div = document.createElement('div');
         div.innerHTML = `
-      <a onclick="handleMusic('${category.category_id}')" class="bg-gray-200 py-1 px-3 text-lg cursor-pointer">${category.category}</a>`;
+      <a onclick="handleMusic('${category.category_id}')" class="bg-gray-200 py-1 active:bg-red-400 px-3 text-lg cursor-pointer">${category.category}</a>`;
         dataContainer.appendChild(div);
     });
 
@@ -21,30 +21,45 @@ const handleMusic = async (categoryId) => {
     const res = await fetch(` https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json()
     const musics = data.data;
-    console.log(musics);
+    // console.log(musics);
+
+    if (musics.length == '0') {
+        const errorContainer = document.getElementById('error-container');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img class="w-40 mx-auto" src="./img/Icon.png" alt=""> 
+        <p class="w-[260px] text-3xl">Oops!! Sorry, There is no content here</p> `;
+        errorContainer.appendChild(div);
+
+    }
+    // else {
+    //     errorContainer.innerText = '';
+    // }
+
+
     const musicsContainer = document.getElementById('musics-container');
     musicsContainer.innerText = '';
 
     musics.forEach((music) => {
-        console.log(music);
-
-
         const div = document.createElement('div');
-        div.classList = `card  bg-base-100 shadow-xl`;
+        div.classList = `card bg-gray-100 shadow-xl`;
         div.innerHTML = `
-        <figure><img class="h-44 w-full" src="${music.thumbnail}" alt="thumbnail" /></figure>
+        <figure><img class="h-44 w-full" src="${music?.thumbnail}" alt="thumbnail" /></figure>
                 <div class="card-body">
               
                     <div class="card-title">
-                    <figure><img class="h-10 w-10 rounded-full" src="${music.authors[0].profile_picture}" alt="thumbnail" /></figure>
-                        <h2 class="">${music.authors[0].profile_name}</h2>
+                    <figure><img class="h-10 w-10 rounded-full" src="${music.authors[0]?.profile_picture}" alt="thumbnail" /></figure>
+                        <h2 class="text-gary-400">${music.authors[0]?.profile_name}</h2>
                     </div>
-                    <p>${music.others.views} views</p>
+                    <p>${music?.title
+            } </p>
+                    <p>${music?.others?.views} views</p>
                  
                 </div>
             </div>
         `;
         musicsContainer.appendChild(div);
+
     })
 
 }
